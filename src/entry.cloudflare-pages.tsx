aa06f5461e3dac0e683036493b605fd7d4052961
Ethan Glover <hello@ethang.email>
2023-12-15 19:43:31 -0600

    Add Cloudflare Pages support and update dependencies
    The commit includes the creation of a new entry point for Cloudflare Pages along with the necessary adapters. It also involves adding headers and redirects configurations. Moreover, the dependencies in 'pnpm-lock.yaml' have been updated, including the addition of Cloudflare specific packages.

---
+++ src/entry.cloudflare-pages.tsx
@@ -0,0 +1,29 @@
+/*
+ * WHAT IS THIS FILE?
+ *
+ * It's the entry point for Cloudflare Pages when building for production.
+ *
+ * Learn more about the Cloudflare Pages integration here:
+ * - https://qwik.builder.io/docs/deployments/cloudflare-pages/
+ *
+ */
+import {
+  createQwikCity,
+  type PlatformCloudflarePages,
+  // eslint-disable-next-line import/no-unresolved
+} from '@builder.io/qwik-city/middleware/cloudflare-pages';
+// eslint-disable-next-line import/no-unresolved
+import qwikCityPlan from '@qwik-city-plan';
+// eslint-disable-next-line import/no-unresolved
+import { manifest } from '@qwik-client-manifest';
+
+import render from './entry.ssr';
+
+declare global {
+  // eslint-disable-next-line @typescript-eslint/consistent-type-definitions
+  interface QwikCityPlatform extends PlatformCloudflarePages {}
+}
+
+const fetch = createQwikCity({ manifest, qwikCityPlan, render });
+
+export { fetch };
