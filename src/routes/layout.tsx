4cd6bfd92ce342454ab1e143082350c09b33261b
Ethan Glover <hello@ethang.email>
2023-12-15 18:21:31 -0600

    Update layout and navigation module with routing functionality
    This commit introduces improved routing functionality by adding 'routeLoader$' and 'useNavigate' to the layout module along with the 'RouteNavigate' type to the navigation component. The layout now dynamically retrieves the page pathname and passes it along with the navigation function to the Navigation component via props. Furthermore, an instance of 'NextUIProvider' now encapsulates the 'Navbar' component in the navigation module, allowing enhanced link navigation throughout the application.

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,5 +1,6 @@
 import { component$, Slot } from '@builder.io/qwik';
 import type { RequestHandler } from '@builder.io/qwik-city';
+import { routeLoader$, useNavigate } from '@builder.io/qwik-city';
 
 import { Navigation } from '../integrations/navigation';
 
@@ -10,10 +11,21 @@ export const onGet: RequestHandler = async ({ cacheControl }) => {
   });
 };
 
+export const usePathname = routeLoader$(({ pathname }) => {
+  return pathname;
+});
+
 export default component$(() => {
+  const navigate = useNavigate();
+  const pathname = usePathname();
+
   return (
     <>
-      <Navigation imagesAmount={0} pathName="/" />
+      <Navigation
+        imagesAmount={0}
+        navigate={navigate}
+        pathName={pathname.value}
+      />
       <main>
         <Slot />
       </main>
