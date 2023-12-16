f11fd05a3ce26443e932ded37ea1c653fe151fcf
Ethan Glover <hello@ethang.email>
2023-12-15 19:29:38 -0600

    Add new routes and components for file handling
    Introduced a new route '/files' and added supportive components like `DocumentLink` for better data structuring. These files handle data query using `getFiles` and implement document links for covenants and meeting minutes. This enhances the website's structure by segregating the file-related actions into distinct modules, ensuring easier updates and better readability.

---
+++ src/components/document-link.tsx
@@ -0,0 +1,24 @@
+import { component$ } from '@builder.io/qwik';
+import { qwikify$ } from '@builder.io/qwik-react';
+import { Link as NextUILink } from '@nextui-org/react';
+
+import type { FileSchema } from '../sanity/queries/get-files';
+
+const QLink = qwikify$(NextUILink);
+
+export const DocumentLink = component$(
+  ({ document }: { readonly document: FileSchema }) => {
+    return (
+      <div class="flex flex-wrap items-center gap-1 py-2">
+        <QLink
+          isExternal
+          showAnchorIcon
+          className="text-black underline"
+          href={document.file.asset.url}
+        >
+          {document.title}
+        </QLink>
+      </div>
+    );
+  },
+);
---
+++ src/routes/files/index.tsx
@@ -0,0 +1,51 @@
+import { component$ } from '@builder.io/qwik';
+import type { DocumentHead } from '@builder.io/qwik-city';
+import { routeLoader$ } from '@builder.io/qwik-city';
+
+import { Container } from '../../components/container';
+import { DocumentLink } from '../../components/document-link';
+import { getFiles } from '../../sanity/queries/get-files';
+
+export const head: DocumentHead = {
+  meta: [
+    {
+      content: 'Covenants and files for Sterett Creek Village Trustee',
+      name: 'description',
+    },
+  ],
+  title: 'Sterett Creek Village Trustee | Files',
+};
+
+export const useFiles = routeLoader$(() => {
+  return getFiles();
+});
+
+export default component$(() => {
+  const data = useFiles();
+
+  return (
+    <Container>
+      <div class="grid gap-4 md:grid-cols-2">
+        <div>
+          <div>
+            <h2 class="text-2xl font-bold">Files</h2>
+            {data.value.covenants.map(covenant => {
+              return <DocumentLink document={covenant} key={covenant._id} />;
+            })}
+            {data.value.general.map(file => {
+              return <DocumentLink document={file} key={file._id} />;
+            })}
+          </div>
+        </div>
+        <div>
+          <h2 class="text-2xl font-bold">Meeting Minutes</h2>
+          {data.value.meetingMinutes.map(meetingMinute => {
+            return (
+              <DocumentLink document={meetingMinute} key={meetingMinute._id} />
+            );
+          })}
+        </div>
+      </div>
+    </Container>
+  );
+});
