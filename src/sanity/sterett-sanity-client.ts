680ccdf7f5433a71a69c2b74173425dbdbe67dd0
Ethan Glover <hello@ethang.email>
2023-12-15 19:40:45 -0600

    Remove `environment.ts` and hardcode sanity client config
    This commit deletes the `environment.ts` file and directly includes project details in the `sterett-sanity-client.ts` file. It simplifies the process to setup sanity client by removing the need to export and import various values from another file, making the code cleaner and more readable.

--- src/sanity/sterett-sanity-client.ts
+++ src/sanity/sterett-sanity-client.ts
@@ -1,15 +1,12 @@
 import { createClient } from '@sanity/client';
 
-import { environment } from '../util/environment';
-
 export const STERETT_GROQ_API_VERSION = new Date().toISOString().split('T')[0];
 
 export const NO_DRAFTS = "!(_id in path('drafts.**'))";
 
 export const sterettSanityClient = createClient({
   apiVersion: STERETT_GROQ_API_VERSION,
-  dataset: environment.STERETT_SANITY_DATASET,
-  projectId: environment.STERETT_SANITY_PROJECT_ID,
-  token: environment.STERETT_SANITY_RO_TOKEN,
+  dataset: 'production',
+  projectId: '540gjnt8',
   useCdn: true,
 });
