0e7dc09675fcbb88abbdbb739d9537a947f0d6fc
Ethan Glover <hello@ethang.email>
2023-12-15 19:48:19 -0600

    Update dependencies and remove unused packages
    The dependencies are updated, and numerous redundant packages notably associated with 'emotion', 'babel' and 'MUI' (Material-UI) have been purged from the pnpm-lock.yaml. This serves to declutter the project, and could potentially enhance installation times by reducing the number of unnecessary dependencies.

--- src/sanity/sterett-sanity-client.ts
+++ src/sanity/sterett-sanity-client.ts
@@ -1,11 +1,9 @@
 import { createClient } from '@sanity/client';
 
-export const STERETT_GROQ_API_VERSION = new Date().toISOString().split('T')[0];
-
 export const NO_DRAFTS = "!(_id in path('drafts.**'))";
 
 export const sterettSanityClient = createClient({
-  apiVersion: STERETT_GROQ_API_VERSION,
+  apiVersion: '1',
   dataset: 'production',
   projectId: '540gjnt8',
   useCdn: true,
