f928cb22cd5ea0805e265fccec055ee6f75c1161
Ethan Glover <hello@ethang.email>
2024-03-10 13:10:14 -0500

    flatten news and events

--- src/sanity/queries/get-news-and-events.ts
+++ src/sanity/queries/get-news-and-events.ts
@@ -78,10 +78,12 @@ export const sortNewsAndEvents = (
     );
   }
 
-  return merged.sort((a, b) => {
-    const aDate = 'startsAt' in a ? new Date(a.startsAt) : new Date(a.date);
-    const bDate = 'startsAt' in b ? new Date(b.startsAt) : new Date(b.date);
+  return merged
+    .sort((a, b) => {
+      const aDate = 'startsAt' in a ? new Date(a.startsAt) : new Date(a.date);
+      const bDate = 'startsAt' in b ? new Date(b.startsAt) : new Date(b.date);
 
-    return aDate.getTime() - bDate.getTime();
-  });
+      return aDate.getTime() - bDate.getTime();
+    })
+    .flat();
 };
