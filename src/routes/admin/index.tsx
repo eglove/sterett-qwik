63c910c77c51f7d42d364cc15808911964d5fae4
Ethan Glover <hello@ethang.email>
2024-03-25 16:13:21 -0500

    lint

--- src/routes/admin/index.tsx
+++ src/routes/admin/index.tsx
@@ -2,7 +2,7 @@ import type { RequestHandler } from '@builder.io/qwik-city';
 import { HTTP_STATUS } from '@ethang/toolbelt/constants/http';
 
 export const onGet: RequestHandler = ({ redirect }) => {
-  // eslint-disable-next-line @typescript-eslint/no-throw-literal
+  // eslint-disable-next-line @typescript-eslint/no-throw-literal,@typescript-eslint/only-throw-error
   throw redirect(
     HTTP_STATUS.FOUND,
     'https://admin.sterettcreekvillagetrustee.com/',
